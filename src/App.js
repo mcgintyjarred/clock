12ea38fe22efa7b183d962bb05dcc0e3506ee8e1
Jarred Gordon Mcginty <22727922@sun.ac.za>
2021-01-08 09:59:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [time, setTime] = useState(new Date())
+
+    // Get new time every second
+    useEffect(() => {
+        // Get new current time
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000 );
+
+        return () => {
+            clearInterval(interval);
+        };
+
+    }, []);
+
+    // Displayed
+    return (
+        <div className="App">
+        <h1>
+        {
+            time.toLocaleTimeString()
+        }
+        <h1>
+        </div>
+    );
 }
 
 export default App;
